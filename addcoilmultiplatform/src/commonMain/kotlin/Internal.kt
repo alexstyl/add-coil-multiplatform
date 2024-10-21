1a9fc2d340ab16a99b2a4133ca69d76b52074623
alexstyl <1665273+alexstyl@users.noreply.github.com>
2024-10-21 21:45:02 +0700

    Test

--- addcoilmultiplatform/src/commonMain/kotlin/Internal.kt
+++ addcoilmultiplatform/src/commonMain/kotlin/Internal.kt
@@ -1,4 +1,4 @@
 package com.alexstyl.addcoilmultiplatform
 
-// this exists because we cant publish an empty sourceSet
+// this exists because we cannot publish an empty sourceSet
 internal object ForeverAlone
