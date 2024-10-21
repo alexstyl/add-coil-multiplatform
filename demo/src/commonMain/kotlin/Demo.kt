1e7ad235cd8d9034a5ec9b2ffda11cf30c73eba4
alexstyl <1665273+alexstyl@users.noreply.github.com>
2024-10-21 22:16:34 +0700

    Update demo

--- demo/src/commonMain/kotlin/Demo.kt
+++ demo/src/commonMain/kotlin/Demo.kt
@@ -14,9 +14,11 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import coil3.ImageLoader
 import coil3.compose.AsyncImage
+import coil3.compose.LocalPlatformContext
 import coil3.compose.setSingletonImageLoaderFactory
 import coil3.memory.MemoryCache
 import coil3.request.CachePolicy
+import coil3.request.ImageRequest
 import coil3.request.crossfade
 
 @Composable
@@ -27,35 +29,19 @@ fun CoilDemo() {
         }.crossfade(true).build()
     }
 
-    val images = listOf(
-        "https://plus.unsplash.com/premium_photo-1675518474491-a8264226b389?q=80&w=3482&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D?q=80&w=1024",
-        "https://images.unsplash.com/photo-1500462918059-b1a0cb512f1d?q=80&w=3223&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D?q=80&w=1024",
-        "https://images.unsplash.com/photo-1485163819542-13adeb5e0068?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1024",
-        "https://plus.unsplash.com/premium_photo-1669751995015-8e74da994ba5?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1024",
-        "https://images.unsplash.com/photo-1516796181074-bf453fbfa3e6?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1024",
-        "https://images.unsplash.com/photo-1504309092620-4d0ec726efa4?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1024",
-        "https://images.unsplash.com/photo-1506792006437-256b665541e2?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1024",
-        "https://images.unsplash.com/photo-1502691876148-a84978e59af8?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1024",
-        "https://images.unsplash.com/photo-1551410224-699683e15636?q=80&w=3116&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1024",
-        "https://plus.unsplash.com/premium_photo-1675518474491-a8264226b389?q=80&w=3482&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D?q=80&w=1024",
-        "https://images.unsplash.com/photo-1500462918059-b1a0cb512f1d?q=80&w=3223&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D?q=80&w=1024",
-        "https://images.unsplash.com/photo-1485163819542-13adeb5e0068?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1024",
-        "https://plus.unsplash.com/premium_photo-1669751995015-8e74da994ba5?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1024",
-        "https://images.unsplash.com/photo-1516796181074-bf453fbfa3e6?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1024",
-        "https://images.unsplash.com/photo-1504309092620-4d0ec726efa4?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1024",
-        "https://images.unsplash.com/photo-1506792006437-256b665541e2?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1024",
-        "https://images.unsplash.com/photo-1502691876148-a84978e59af8?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1024",
-        "https://images.unsplash.com/photo-1551410224-699683e15636?q=80&w=3116&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1024",
-        "https://plus.unsplash.com/premium_photo-1675518474491-a8264226b389?q=80&w=3482&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D?q=80&w=1024",
-        "https://images.unsplash.com/photo-1500462918059-b1a0cb512f1d?q=80&w=3223&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D?q=80&w=1024",
-        "https://images.unsplash.com/photo-1485163819542-13adeb5e0068?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1024",
-        "https://plus.unsplash.com/premium_photo-1669751995015-8e74da994ba5?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1024",
-        "https://images.unsplash.com/photo-1516796181074-bf453fbfa3e6?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1024",
-        "https://images.unsplash.com/photo-1504309092620-4d0ec726efa4?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1024",
-        "https://images.unsplash.com/photo-1506792006437-256b665541e2?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1024",
-        "https://images.unsplash.com/photo-1502691876148-a84978e59af8?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1024",
-        "https://images.unsplash.com/photo-1551410224-699683e15636?q=80&w=3116&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1024",
-    )
+    val images = List(90) {
+        listOf(
+            "https://plus.unsplash.com/premium_photo-1675518474491-a8264226b389?q=80&w=3482&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D?q=80&w=1024",
+            "https://images.unsplash.com/photo-1500462918059-b1a0cb512f1d?q=80&w=3223&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D?q=80&w=1024",
+            "https://images.unsplash.com/photo-1485163819542-13adeb5e0068?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1024",
+            "https://plus.unsplash.com/premium_photo-1669751995015-8e74da994ba5?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1024",
+            "https://images.unsplash.com/photo-1516796181074-bf453fbfa3e6?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1024",
+            "https://images.unsplash.com/photo-1504309092620-4d0ec726efa4?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1024",
+            "https://images.unsplash.com/photo-1506792006437-256b665541e2?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1024",
+            "https://images.unsplash.com/photo-1502691876148-a84978e59af8?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1024",
+            "https://images.unsplash.com/photo-1551410224-699683e15636?q=80&w=3116&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1024",
+        )
+    }.flatten()
     LazyVerticalGrid(
         GridCells.Fixed(3),
         horizontalArrangement = Arrangement.spacedBy(4.dp),
@@ -63,10 +49,13 @@ fun CoilDemo() {
     ) {
         items(images) { image ->
             AsyncImage(
-                model = image,
+                model = ImageRequest.Builder(LocalPlatformContext.current)
+                    .data(image)
+                    .crossfade(true)
+                    .build(),
                 contentDescription = null,
                 modifier = Modifier.clip(RoundedCornerShape(4.dp)).fillMaxSize().height(200.dp),
-                contentScale = ContentScale.Crop
+                contentScale = ContentScale.Crop,
             )
         }
     }
